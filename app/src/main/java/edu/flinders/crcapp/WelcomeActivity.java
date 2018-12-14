862f843e45f24c35d2631d8daac1793bd45718bb
Tri Phan <Phan@stream.local>
2018-12-14 10:45:55 +1030

    Initial commit

---
+++ app/src/main/java/edu/flinders/crcapp/WelcomeActivity.java
@@ -0,0 +1,22 @@
+/*
+ * WelcomeActivity.java on CRCApp
+ *
+ * Created by Tri Phan on 14/12/18 10:27 AM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 14/12/18 10:27 AM
+ */
+
+package edu.flinders.crcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
